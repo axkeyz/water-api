92070550ea59c288d7a3336f8961805bf5172375
axkeyz <xahkun@gmail.com>
2021-11-10 15:13:58 +1300

    Add cronjob to update database

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.4
+	github.com/robfig/cron v1.2.0
 )
--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import (
     "github.com/axkeyz/water-down-again/api"
+	"github.com/robfig/cron"
+	"fmt"
 )
 
-func main() {
+func UpdateOutages() {
 	outages := api.GetAPIData()
 	api.WriteOutage(outages)
+	fmt.Printf("Done.\n")
+}
+
+func main() {
+	// Retrieve & write from Watercare API to this app's database for the first time
+	UpdateOutages()
+
+	// Create cronjob to run retrieval & write every 45 minutes
+	c := cron.New()
+    c.AddFunc("@every 45m", UpdateOutages)
+    c.Start()
+    select {}
 }
