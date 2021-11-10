ea748d72f71f7c0e39d4659f773ca2a19ff8ddf6
axkeyz <xahkun@gmail.com>
2021-11-10 16:05:11 +1300

    Add router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/axkeyz/water-down-again
 go 1.13
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.4
 	github.com/robfig/cron v1.2.0
--- main.go
+++ main.go
@@ -3,22 +3,27 @@ package main
 import (
     "github.com/axkeyz/water-down-again/api"
 	"github.com/robfig/cron"
-	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
-
-func UpdateOutages() {
-	outages := api.GetAPIData()
-	api.WriteOutage(outages)
-	fmt.Printf("Done.\n")
-}
 
 func main() {
 	// Retrieve & write from Watercare API to this app's database for the first time
-	UpdateOutages()
+	api.UpdateOutages()
+
+	// Init the mux router
+    router := mux.NewRouter()
+
+	// Setup routes
+	router.HandleFunc("/api", api.GetOutages).Methods("GET")
+
+	// Run server
+	log.Fatal(http.ListenAndServe("localhost:8553", router))
 
 	// Create cronjob to run retrieval & write every 45 minutes
 	c := cron.New()
-    c.AddFunc("@every 45m", UpdateOutages)
+    c.AddFunc("@every 45m", api.UpdateOutages)
     c.Start()
     select {}
 }
