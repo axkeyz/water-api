83b11e7f92077c4b461374fd7f04bfb078875ee1
axkeyz <xahkun@gmail.com>
2021-11-16 00:53:08 +1300

    Update cronjob logic #6
    - Removed package
    - go mod tidy
    
    Resolves #6

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/axkeyz/water-down-again/api"
-	"github.com/robfig/cron"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	log.Println("Server is running")
+	
+	// Create a cronjob for every hour
+	go func() {
+		for {
+			api.UpdateOutages()
+			<-time.After(1 * time.Hour)
+		}
+	}()
+	
 	// Retrieve & write from Watercare API to this app's database for the first time
 	api.UpdateOutages()
 
@@ -21,13 +31,4 @@ func main() {
 
 	// Run server
 	log.Println(http.ListenAndServe(":8080", router))
-	log.Println("Server is running")
-
-	// Create cronjobs
-	c := cron.New()
-	// Retrieve Watercare API & update app database
-	c.AddFunc("@hourly", api.UpdateOutages)
-
-	c.Start()
- 	select {}
 }
