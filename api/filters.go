b7296f524bdcee6dff3668e900daeb68795a10ef
axkeyz <xahkun@gmail.com>
2022-07-30 02:30:39 +1200

    Fix default limit-offset bug
    - Fixed an issue where the limit and offset would not be set by
    default

--- api/filters.go
+++ api/filters.go
@@ -12,14 +12,14 @@ import (
 func MakeFilterQuery(r *http.Request) (string, string) {
 	// Get params
 	params := r.URL.Query()
-	var filter, ordering string
+	var filter string
+
+	query := new(Query)
+
+	ordering := query.MakeOrderbyPaginationString(params)
 
 	// if parameters exist
 	if len(params) > 0 {
-		query := new(Query)
-
-		ordering = query.MakeOrderbyPaginationString(params)
-
 		query.SetWheres(params)
 		if len(query.Wheres) > 0 {
 			filter = query.MakeWhereString(GetSQLCondition(params.Get("excl")))
